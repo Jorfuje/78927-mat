b96f6e0fe427eddc9ff46a7185032df7dd213874
Jorfuje <arbjorfuje@gmail.com>
2023-02-28 11:12:37 -0600

    Tarea

--- practica02/Services/Operaciones.cs
+++ practica02/Services/Operaciones.cs
@@ -1,18 +1,31 @@
 using System;
 using WSDL.mensajes;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace WSDL.operaciones
 {
     public class Operaciones : Mensajes
     {
+         List<string> personNames = new List<string>();
         public string Saludar(string nombre)
         {
             string msj = "Hola " + nombre;
+            personNames.Add(nombre);
+            foreach(string item in personNames)
+            {
+                Console.WriteLine(item);
+            }
             return msj; 
+            
+            
         }
         public string Mostrar(int id)
         {
-            return "x";
+            //string name = personNames[id];
+            return personNames[id];
         }
     }
 }
