fc3e52e13f1c1fa573067cdbf9d353b65dac57c5
Jorfuje <arbjorfuje@gmail.com>
2023-04-18 11:11:58 -0600

    Mini proyecto WS

---
+++ inventario/target/generated-sources/jaxb/https/t4is_uv_mx/inventario/ObjectFactory.java
@@ -0,0 +1,103 @@
+//
+// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.3.2 
+// Visite <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2023.04.14 a las 10:56:09 PM CST 
+//
+
+
+package https.t4is_uv_mx.inventario;
+
+import javax.xml.bind.annotation.XmlRegistry;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the https.t4is_uv_mx.inventario package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: https.t4is_uv_mx.inventario
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link RegistrarRequest }
+     * 
+     */
+    public RegistrarRequest createRegistrarRequest() {
+        return new RegistrarRequest();
+    }
+
+    /**
+     * Create an instance of {@link RegistrarResponse }
+     * 
+     */
+    public RegistrarResponse createRegistrarResponse() {
+        return new RegistrarResponse();
+    }
+
+    /**
+     * Create an instance of {@link ModificarRequest }
+     * 
+     */
+    public ModificarRequest createModificarRequest() {
+        return new ModificarRequest();
+    }
+
+    /**
+     * Create an instance of {@link ModificarResponse }
+     * 
+     */
+    public ModificarResponse createModificarResponse() {
+        return new ModificarResponse();
+    }
+
+    /**
+     * Create an instance of {@link MostrarRequest }
+     * 
+     */
+    public MostrarRequest createMostrarRequest() {
+        return new MostrarRequest();
+    }
+
+    /**
+     * Create an instance of {@link MostrarResponse }
+     * 
+     */
+    public MostrarResponse createMostrarResponse() {
+        return new MostrarResponse();
+    }
+
+    /**
+     * Create an instance of {@link EliminarRequest }
+     * 
+     */
+    public EliminarRequest createEliminarRequest() {
+        return new EliminarRequest();
+    }
+
+    /**
+     * Create an instance of {@link EliminarResponse }
+     * 
+     */
+    public EliminarResponse createEliminarResponse() {
+        return new EliminarResponse();
+    }
+
+}
---
+++ inventario/target/generated-sources/jaxb/https/t4is_uv_mx/inventario/RegistrarRequest.java
@@ -0,0 +1,118 @@
+//
+// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.3.2 
+// Visite <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2023.04.14 a las 10:56:09 PM CST 
+//
+
+
+package https.t4is_uv_mx.inventario;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para anonymous complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="nombre" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="categoria" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="cantidad" type="{http://www.w3.org/2001/XMLSchema}int"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "nombre",
+    "categoria",
+    "cantidad"
+})
+@XmlRootElement(name = "RegistrarRequest")
+public class RegistrarRequest {
+
+    @XmlElement(required = true)
+    protected String nombre;
+    @XmlElement(required = true)
+    protected String categoria;
+    protected int cantidad;
+
+    /**
+     * Obtiene el valor de la propiedad nombre.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Define el valor de la propiedad nombre.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setNombre(String value) {
+        this.nombre = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad categoria.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getCategoria() {
+        return categoria;
+    }
+
+    /**
+     * Define el valor de la propiedad categoria.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setCategoria(String value) {
+        this.categoria = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad cantidad.
+     * 
+     */
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    /**
+     * Define el valor de la propiedad cantidad.
+     * 
+     */
+    public void setCantidad(int value) {
+        this.cantidad = value;
+    }
+
+}
