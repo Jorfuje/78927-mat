5c1497c71a61bcbe818a51b0bdd922792cb8f06e
jorge <arbjorfuje@gmail.com>
2023-04-25 15:04:52 -0600

    Rest Spring

---
+++ RestSpring/demo/src/main/java/mx/uv/demo/DemoApplication.java
@@ -0,0 +1,66 @@
+package mx.uv.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String name () {
+		return "Hola Mundo!";
+	}
+
+	@RequestMapping("/adios")
+	public String name2 () {
+		return "<h1>adios mundo</h1>";
+	}
+
+	@RequestMapping("/pregunta")
+	public String name3 () {
+		return "<h1>cómo estás?</h1>";
+	}
+
+	@RequestMapping("/productos")
+	public List<String> productos() {
+		List<String> p = new ArrayList<String>();
+		p.add("pambazos");
+		p.add("tamales");
+		p.add("refrescos");
+		return p;
+	}
+
+	@RequestMapping("/productos2")
+	public List<Productos> productos2() {
+		List<Productos> lista = new ArrayList<Productos>();
+		Productos p0 = new Productos("pambazos", 10);
+		lista.add(p0);
+		Productos p1 = new Productos("tamales", 10);
+		lista.add(p1);
+		Productos p2 = new Productos("refrescos", 10);
+		lista.add(p2);
+		return lista;
+	}
+
+	@RequestMapping(value = "/get", method = RequestMethod.GET)
+	public String saludarGet(){
+		return "Mensaje de tipo get";
+	}
+
+	@RequestMapping(value = "/post", method = RequestMethod.POST)
+	public String saludarpost(){
+		return "Mensaje de tipo post";
+	}
+
+}
